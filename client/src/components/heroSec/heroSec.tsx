9f8f6babbf324162a086975337e0ad03a762e61e
Hammad Ali <hammadali.cse123@gmail.com>
2026-04-17 17:32:04 +0500

    Landing Page Completed

--- client/src/components/heroSec/heroSec.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const HeroSec = () => {
-    return (
-        <div>HeroSec</div>
-    )
-}
-
-export default HeroSec
--- client/src/pages/landing-page.tsx
+++ client/src/pages/landing-page.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from 'react';
+import Navbar from '../components/navbar/navbar';
+import Hero from '../components/hero/hero';
+import Stats from '../components/stats/stats';
+import Safety from '../components/safety/safety';
+import Services from '../components/services/services';
+import Process from '../components/process/process';
+import FeatureCards from '../components/featureCards/featureCards';
+import Testimonials from '../components/testimonials/testimonials';
+import CTA from '../components/cta/cta';
+import Footer from '../components/footer/footer';
 
 const LandingPage = () => {
     return (
-        <div className='text-red-600'>LandingPage</div>
-    )
-}
+        <div className="bg-white">
+            <Navbar />
+            <Hero />
+            <Stats />
+            <Safety />
+            <Services />
+            <Process />
+            <FeatureCards />
+            <Testimonials />
+            <CTA />
+            <Footer />
+        </div>
+    );
+};
 
-export default LandingPage
+export default LandingPage;
